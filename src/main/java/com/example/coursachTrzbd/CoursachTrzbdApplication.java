b59accf4ea47ef8f7c5880ef6d8f5568fb89f6ac
sergey <sergey128476@gmail.com>
2025-05-22 21:24:32 +0300

    Creating a Project

---
+++ src/main/java/com/example/coursachTrzbd/CoursachTrzbdApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursachTrzbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursachTrzbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursachTrzbdApplication.class, args);
+	}
+
+}
